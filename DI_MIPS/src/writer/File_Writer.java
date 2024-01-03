3176321a226172e10f1aac2e86c17a7bdc7dd689
thevasi1 <thevasi1@abv.bg>
2024-01-03 14:41:57 +0100

    feat: add first sketch of file writer

--- DI_MIPS/src/writer/File_Writer.java
+++ DI_MIPS/src/writer/File_Writer.java
@@ -7,6 +7,16 @@ package writer;
 
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.List;
+
+
+import instruction.Operator;
+import instruction.Stages;
 
 /**
  *
@@ -14,16 +24,59 @@ import java.io.IOException;
  */
 public class File_Writer {
     
-    private FileWriter writer; 
+    private List<Map<Integer, Stages>> stages;
+    private Map<Integer, Operator> instructionsOperators;
+    private Map<Integer, Integer> instructionsStart;
 
-    File_Writer(){
-        String fileName = "../output.txt";
+    private FileWriter writer;
+    private int cyclesCount;
+
+    public File_Writer(){
+        this.stages = new ArrayList<>();
+
+        this.instructionsOperators = new LinkedHashMap<Integer, Operator>(); 
+        this.instructionsStart = new HashMap<Integer, Integer>(); 
+
+        this.cyclesCount = 0;
+        String fileName = "./output.txt";
+
         try {
             this.writer = new FileWriter(fileName);
         } catch (IOException e) {
             System.out.println(e.getMessage());
             e.printStackTrace();
         }
+    }
+
+    public void writeStage(int cycle, int instructionId, Stages stage, Operator operator){
+        if (cycle > this.cyclesCount) {
+            this.cyclesCount = cycle;
+        }
+
+        if (!this.instructionsOperators.containsKey(instructionId)){
+            Map<Integer, Stages> newInstructionRow = new HashMap<Integer, Stages>();
+            newInstructionRow.put(cycle, stage);
+
+            this.stages.add(newInstructionRow);
+
+            this.instructionsOperators.put(instructionId, operator);
+            this.instructionsStart.put(instructionId, cycle);
+
+            return;
+        }
+
+        Iterator<Integer> it = this.instructionsOperators.keySet().iterator();
+        int instructionOrder = 0;
+
+        while (it.hasNext()){
+            if (it.next().equals(instructionId)) {
+                System.out.println("We get instructionId of " + instructionOrder);
+                break;
+            }
+            instructionOrder++;
+        }
+           
+        this.stages.get(instructionOrder).put(cycle, stage);
     }
 
     void writeLineToFile(String line){
@@ -37,10 +90,68 @@ public class File_Writer {
         }
     }
 
+    void printMap(){
+        for (Map.Entry<Integer, Operator> entry : this.instructionsOperators.entrySet()) {
+            System.out.println("Key: " + entry.getKey() + ", Value: " + entry.getValue());
+        }
+    }
+
+    void printFile() {
+        // print cycle top
+        String TRIPPLE_SPACE = "   ";
+        String QUADRAPLE_SPACE = "    ";
+
+        StringBuilder cyclesHeader = new StringBuilder(TRIPPLE_SPACE + QUADRAPLE_SPACE);
+        for (int i = 0; i < this.cyclesCount; i++) {
+            cyclesHeader.append(i + 1);
+            cyclesHeader.append(TRIPPLE_SPACE);
+        }
+        cyclesHeader.append('\n');
+        this.writeLineToFile(cyclesHeader.toString());
+
+        //print instructions
+        int instructionOrder = 0;
+        for (Map.Entry<Integer, Operator> entry : this.instructionsOperators.entrySet()) {
+            int instructionId = entry.getKey();
+            String instructionName = entry.getValue().toString();
+
+            int instructionCycleStart = this.instructionsStart.get(instructionId);
+            StringBuilder instructionLine = new StringBuilder(instructionName);
+
+            // add empty spaces untill start of cycle
+            for (int i = 0; i <= instructionCycleStart; i++) {
+                instructionLine.append(QUADRAPLE_SPACE);
+            }
+
+            // add stages
+            Map<Integer, Stages> instructionStages = this.stages.get(instructionOrder);
+
+            Iterator<Integer> it = instructionStages.keySet().iterator();
+            while (it.hasNext()) {
+                String stage = instructionStages.get(instructionCycleStart).toString();
+
+                instructionLine.append(stage);
+                instructionLine.append(TRIPPLE_SPACE);
+
+                instructionCycleStart++;
+                it.next();
+            }
+
+            // write
+            instructionLine.append('\n');
+            this.writeLineToFile(instructionLine.toString());
+
+            // increment
+            instructionOrder++;
+        }
+    }
+
     @Override
-    protected void finalize() {
+    public void finalize() {
         try {
-            this.writer.close(); 
+            this.printMap();
+            this.printFile();
+            this.writer.close();
         } catch (IOException e) {
             System.out.println(e.getMessage());
             e.printStackTrace();
